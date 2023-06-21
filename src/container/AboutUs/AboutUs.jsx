d78931841e8d5cf618571f8c540697f25bc2efd7
Mamatha Aluvala <ukiyo_kira@Mamatha.local>
2023-06-21 16:56:23 +0530

    first commit

---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
+import "./AboutUs.css";
+
+const AboutUs = () => (
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="g letter" />
+    </div>
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis,
+          tenetur! Explicabo voluptatibus totam consequuntur, numquam earum,
+          minus ducimus voluptates, fuga a dolores molestiae odio provident!
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="knife" />
+      </div>
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis,
+          tenetur! Explicabo voluptatibus totam consequuntur, numquam earum,
+          minus ducimus voluptates, fuga a dolores molestiae odio provident!
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+    </div>
+  </div>
+);
+
+export default AboutUs;
---
+++ src/container/Laurels/Laurels.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { images, data } from "../../constants";
+import { SubHeading } from "../../components";
+import logo from "../../assets/logo.png";
+import "./Laurels.css";
+
+const AwardCard = ({ award: { imgUrl, title, subtitle }, key }) => {
+  return (
+    <div className="app__laurels_awards-card">
+      <img src={imgUrl} alt="award" />
+      <div className="app__laurels_awards-card_content">
+        <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+          {title}
+        </p>
+        <p className="p__opensans" style={{ color: "#AAA" }}>
+          {subtitle}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const Laurels = () => (
+  <div className="app__bg app__wrapper section__padding" id="awards">
+    <div className="app__wrapper_info">
+      <div className="app__laurel_logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <SubHeading title="Awards & Recognition" />
+      <h1 className="headtext__cormorant">Our Laurels</h1>
+      <div className="app__laurels_awards">
+        {data.awards.map((award) => (
+          <AwardCard award={award} key={award.title} />
+        ))}
+      </div>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.laurels} alt="laurels" />
+    </div>
+  </div>
+);
+
+export default Laurels;
